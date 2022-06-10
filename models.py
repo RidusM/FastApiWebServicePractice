0cf721dcc866467c56d785699c0e60afc0fae619
RidusM <79109438+RidusM@users.noreply.github.com>
2022-06-10 10:13:59 +0300

    v0.1

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, Date, Time
+from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
 from database import Base
 from typing import TypedDict
 
@@ -10,6 +12,14 @@ time_format= "%H:%M:%S"
 dtt = dt.strftime(time_format)
 dtd = dt.strftime(date_format)
 
+association_table = Table(
+    "objectcars",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("object_id", Integer, ForeignKey("objects.id")),
+    Column("cars_id", Integer, ForeignKey("cars.id")),
+)
+
 class ObjectDict(TypedDict):
     id: int
     title: str
@@ -18,17 +28,30 @@ class ObjectDict(TypedDict):
     longitude: str
     date: str
     time: str
+    capacity: int
 
 class Object(Base):
     __tablename__ = "objects"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String, index=True, nullable=False)
     date = Column(String)
     time = Column(String)
     location = Column(String, index=True)
     latitude = Column(String)
     longitude = Column(String)
+    capacity = Column(String)
+    car = relationship('cars', secondary=association_table, backref='objects')
+
+class Car(Base):
+    __tablename__ = "cars"
+
+    id = Column(Integer, primary_key=True, index=True)
+    Model = Column(String, nullable=False)
+    Number = Column(String, nullable=False, index=True)
+    Capacity = Column(Integer, nullable=False)
+    object = relationship('objects', secondary=association_table, backref='cars')
+
 
 
     def __init__(self, title:str, location:str, latitude:str, longitude:str, date:dtd, time:dtt):
